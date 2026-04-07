6e03084f165647e5202e4c78e6294b5452ee03d3
srkponnapalli <sivaramakrishna01997@gmail.com>
2026-04-06 23:38:26 -0400

    phase 2 - database connection working

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,23 @@
 from fastapi import FastAPI
 
+from app.db.connection import SessionLocal
+from sqlalchemy import text
+
 app = FastAPI()
 
 
 @app.get("/health")
 def function_name():
     return {"Health": "is_good"}
+
+
+@app.get("/db-health")
+def db_health():
+    try:
+        db = SessionLocal()
+        db.execute(text("Select 1"))
+        return {"DB Health": "is_good"}
+    except Exception as e:
+        return {"DB Health": "is_bad", "error": str(e)}
+    finally:
+        db.close()
